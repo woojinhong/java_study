14a4d96be973db9446f0b3d2d4edb368db780b90
woojin <orolzleim@gmail.com>
2024-08-17 22:58:48 +0900

    multidimensial arrays

---
+++ java_code/src/변수/multiDarray.java
@@ -0,0 +1,14 @@
+package 변수;
+
+public class multiDarray {
+
+    // 3행 4열의 배열을 선언
+    int[][] arr2d = new int[3][4];
+
+    // 3행 4열의 배열을 선언과 초기화
+    int[][] arr2ds = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12}
+    };
+}
